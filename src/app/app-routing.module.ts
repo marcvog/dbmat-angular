83e9a4433c0b11dabf60ab8f17fc4c39ad715790
Marcelo Vogel <mavogel@cern.ch>
2024-08-26 21:49:55 +0200

    Added home area and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UserNotAuthorizedComponent } from './user-not-authorized/user-not-authorized.component';
+import { IntroComponent } from './intro/intro.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component:IntroComponent },
   { path: 'unauthorized', component:UserNotAuthorizedComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
